44d930e06d295036af63df242c70debc93666467
jlin03 <jlin03@stuy.edu>
2018-09-26 17:04:43 -0400

    2nd function written + test cases

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -3,21 +3,29 @@ public class ArrayDemo {
 		int[] test = new int[10];
 		int[][] test2d = new int[10][10];
 		printArray(test);
+		printArray(test2d);
 	}
 	
 	public static void printArray(int[]ary) {
-			String out = "[";
-			for(int i = 0; i < ary.length; i++) {
-				if (i == ary.length - 1) {					
-					out += String.valueOf(ary[i]) + "]";
-				}
-				else {
-					out += String.valueOf(ary[i]) + ", ";
-				}
-				
+		String out = "[";
+		for(int i = 0; i < ary.length; i++) {
+			if (i == ary.length - 1) {					
+				out += String.valueOf(ary[i]) + "]";
 			}
-			System.out.println(out);
+			else {
+				out += String.valueOf(ary[i]) + ", ";
+			}
 		}
-		
-		
+			System.out.println(out);
+	}
+	
+	public static void printArray(int[][]ary) {
+		System.out.println("[");
+		for(int i = 0; i < ary.length; i++) {
+			printArray(ary[i]);
+		}
+		System.out.println("]");
+	}
+	
+	
 }
